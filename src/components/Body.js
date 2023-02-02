87907e7046843ec64b15e77426d41737e988c6e2
ccatack <59943562+ccatack@users.noreply.github.com>
2023-02-01 19:09:54 -0800

    fixed eye blinking feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,13 @@ export default function Body() {
 
     return (
         <div className="bg-blue-200 flex flex-col items-center w-1/2">
-            <div className="flex flex-row justify-center items-center w-1/2 p-3 mt-24">
-                <div className="w-14 h-11 bg-black mr-24 rounded-3xl border-8 border-white hover:h-1"></div>
-                <div className="w-14 h-11 bg-black ml-24 rounded-3xl border-8 border-white hover:h-1"></div>
+            <div className="flex flex-row justify-between items-center w-1/2 p-3 mt-24">
+                <div className="group w-1/5 h-11 rounded-3xl flex items-center">
+                    <div className="w-full h-11 bg-black rounded-3xl border-8 border-white group-hover:h-1"></div>
+                </div>
+                <div className="group w-1/5 h-11 rounded-3xl flex items-center">
+                    <div className="w-full h-11 bg-black rounded-3xl border-8 border-white group-hover:h-1"></div>
+                </div>
             </div>
             <div className="mt-8 bg-blue-400 w-28 text-center">
                 nose
