fdc46f3e7f97addd43607a2722435ebbe3ea1811
ccatack <59943562+ccatack@users.noreply.github.com>
2023-02-09 19:40:44 -0800

    Adding header button functionality
    Header buttons now link to their respective pages

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { Link } from "gatsby";
 
 export default function Header() {
 
+    //CAN ANYBODY DEPLOY TO GHPAGES CUZ THEY HAVE THE DEPLOY SCRIPT?
+
     return (
         <nav className="flex flex-row items-center justify-end bg-gray-200 h-12">
-            <button className="ml-12 text-lg">Home</button>
-            <button className="ml-12 text-lg">My Work</button>
-            <button className="ml-12 text-lg">Resume</button>
-            <button className="ml-12 mr-12 text-lg">About</button>
+            <button className="ml-12 text-lg">
+                <Link to="/">Home</Link>
+            </button>
+            <button className="ml-12 text-lg">
+                <Link to="/Work">My Work</Link>
+            </button>
+            <button className="ml-12 text-lg">
+                <Link to="/Resume">Resume</Link>
+            </button>
+            <button className="ml-12 mr-12 text-lg">
+                <Link to="/About">About</Link>
+            </button>
         </nav>
     )
 }
