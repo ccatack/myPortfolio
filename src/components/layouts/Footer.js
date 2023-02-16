136a173545e958ae42d58256bf3884483a468968
ccatack <59943562+ccatack@users.noreply.github.com>
2023-02-16 15:41:30 -0800

    Adding mages to footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
+import LILogo from "../../images/linkedinLogo.png"
+import GHLogo from "../../images/githubLogo.png"
+import EmailLogo from "../../images/emailIcon.png"
 
 export default function Footer() {
 
     return (
         <div className="bg-white h-12 flex flex-row items-center justify-end">
-            <div className="ml-4 text-xs">Copyright 2023 Corbin Atack</div>
-            <div className="ml-4 w-8 h-8 bg-gray-500 rounded-3xl"></div>
-            <div className="ml-4 w-8 h-8 bg-gray-500 rounded-3xl"></div>
-            <div className="ml-4 mr-4 w-8 h-8 bg-gray-500 rounded-3xl"></div>
+            <div className="text-xs text-gray-400">Copyright 2023 Corbin Atack</div>
+            <img src={LILogo} className="ml-6 h-8 max-h-full"></img>
+            <img src={GHLogo} className="ml-4 h-8 max-h-full"></img>
+            <img src={EmailLogo} className="ml-4 mr-6 h-8 max-h-full"></img>
         </div>
     )
 }
