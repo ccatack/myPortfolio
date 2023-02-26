ee7f5eefdb30ba7294c477b9ff7f1de423312d84
ccatack <59943562+ccatack@users.noreply.github.com>
2023-02-26 13:18:41 -0800

    adding footer functionality
    buttons now provide user feedback when hovering and link to LI GH and email

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -7,10 +7,16 @@ export default function Footer() {
 
     return (
         <div className="bg-white h-12 flex flex-row items-center justify-end">
-            <div className="text-xs text-gray-400">Copyright 2023 Corbin Atack</div>
-            <img src={LILogo} className="ml-6 h-8 max-h-full"></img>
-            <img src={GHLogo} className="ml-4 h-8 max-h-full"></img>
-            <img src={EmailLogo} className="ml-4 mr-6 h-8 max-h-full"></img>
+            <div className="text-xs text-gray-500">Copyright 2023 Corbin Atack</div>
+            <a href="https://www.linkedin.com/in/corbin-atack/" target="_blank">
+                <img src={LILogo} className="ml-6 h-8 max-h-full opacity-40 hover:opacity-80 cursor-pointer"></img>
+            </a>
+            <a href="https://github.com/ccatack" target="_blank">
+                <img src={GHLogo} className="ml-6 h-8 max-h-full opacity-40 hover:opacity-80 cursor-pointer"></img>
+            </a>
+            <a href="mailto:ccatack@gmail.com" target="_blank">
+                <img src={EmailLogo} className="ml-6 mr-6 h-8 max-h-full opacity-40 hover:opacity-80 cursor-pointer"></img>
+            </a>
         </div>
     )
 }
